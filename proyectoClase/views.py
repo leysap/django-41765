0f4c84db3b90537e09aae5071d709df21a9a58c2
Leysa Pozo <leysapozo41@gmail.com>
2022-09-29 13:41:01 -0300

    armado de modelo

--- proyectoClase/views.py
+++ proyectoClase/views.py
@@ -1,7 +1,9 @@
 from re import template
 from django.http import HttpResponse
 from datetime import datetime
-from django.template import Context, Template
+from django.template import Context, Template, loader
+import random
+from home.models import Persona
 
 
 def hola(request):
@@ -18,12 +20,45 @@ def calcular_fecha(request, edad):
     return HttpResponse(f" Tu fecha de nac es: {edad} para la fecha {fecha}")
 
 def mi_template(request):
-    cargar_archivo = open(r"D:\Desktop\clases-python\proyecto-clases\templates\template.html", "r")
+    cargar_archivo = open(r"D:\Desktop\clases-python\proyecto-clases\templates\mi_template.html", "r")
 
     template= Template(cargar_archivo.read())
     cargar_archivo.close()
-    
+
     contexto= Context()
 
     template_renderizado = template.render(contexto)
+    return HttpResponse(template_renderizado)
+
+def tu_template(request, nombre):
+    template = loader.get_template("tu_template.html")
+    template_renderizado= template.render({"persona": nombre})
+
+    return HttpResponse(template_renderizado)
+
+def prueba_template(request):
+    mi_contexto= {"rango":list(range(1,11)),
+                  "valor_aleatorio": random.randrange(1,11)
+                }
+
+    template = loader.get_template("prueba_template.html")
+    template_renderizado= template.render(mi_contexto)
+
+    return HttpResponse(template_renderizado)
+
+def crear_persona(request,nombre,apellido):
+    
+    persona = Persona(nombre=nombre, apellido=apellido, edad=random.randrange(1,99),fecha_nacimiento=datetime.now())
+
+    persona.save()
+    template = loader.get_template("crear_persona.html")
+    template_renderizado= template.render({"personas": persona})
+
+    return HttpResponse(template_renderizado)
+
+def ver_personas(request):
+    personas= Persona.objects.all()
+
+    template = loader.get_template("ver_persona.html")
+    template_renderizado= template.render({"personas": personas})
     return HttpResponse(template_renderizado)
